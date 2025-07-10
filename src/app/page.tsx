b3fc228b0334a3d756e342a08c69c3d62839edd2
ktak <ktak@15inch-MacBook-Air-M3.local>
2025-07-10 14:49:38 +0900

    Home画像変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -327,7 +327,7 @@ export default function HomePage() {
               <div className="@[480px]:p-4">
                 <div
                   className="flex min-h-[480px] flex-col gap-6 bg-cover bg-center bg-no-repeat @[480px]:gap-8 @[480px]:rounded-lg items-start justify-end px-4 pb-10 @[480px]:px-10"
-                  style={{backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.4) 100%), url("https://images.unsplash.com/photo-1560472354-b33ff0c44a43?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1926&q=80")'}}
+                  style={{backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.4) 100%), url("/home_new.png")'}}
                 >
                   <div className="flex flex-col gap-2 text-left">
                     <h1
