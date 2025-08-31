04bdfe77ab30243cc3076f88cd3a4c0ba31fb5da
ktak <ktak@15inch-MacBook-Air-M3.local>
2025-08-31 16:58:57 +0900

    Update works page

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -134,28 +134,6 @@ export default async function WorksPage() {
               ))}
             </div>
 
-            {/* Pagination */}
-            <div className="flex items-center justify-center p-4">
-              <a href="#" className="flex size-10 items-center justify-center">
-                <div className="text-[#141414]" data-icon="CaretLeft" data-size="18px" data-weight="regular">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="currentColor" viewBox="0 0 256 256">
-                    <path d="M165.66,202.34a8,8,0,0,1-11.32,11.32l-80-80a8,8,0,0,1,0-11.32l80-80a8,8,0,0,1,11.32,11.32L91.31,128Z"></path>
-                  </svg>
-                </div>
-              </a>
-              <a className="text-sm font-bold leading-normal tracking-[0.015em] flex size-10 items-center justify-center text-[#141414] rounded-full bg-[#ededed]" href="#">1</a>
-              <a className="text-sm font-normal leading-normal flex size-10 items-center justify-center text-[#141414] rounded-full" href="#">2</a>
-              <a className="text-sm font-normal leading-normal flex size-10 items-center justify-center text-[#141414] rounded-full" href="#">3</a>
-              <span className="text-sm font-normal leading-normal flex size-10 items-center justify-center text-[#141414] rounded-full">...</span>
-              <a className="text-sm font-normal leading-normal flex size-10 items-center justify-center text-[#141414] rounded-full" href="#">10</a>
-              <a href="#" className="flex size-10 items-center justify-center">
-                <div className="text-[#141414]" data-icon="CaretRight" data-size="18px" data-weight="regular">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="currentColor" viewBox="0 0 256 256">
-                    <path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path>
-                  </svg>
-                </div>
-              </a>
-            </div>
           </div>
         </div>
       </div>
