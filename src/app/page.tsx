9129f1e805ecc0ffedd3913061821a74d31ed898
ktak <ktak@15inch-MacBook-Air-M3.local>
2025-08-31 17:57:00 +0900

    Update page.tsx, Footer and Header components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,7 +101,7 @@ export default function HomePage() {
                     <h1
                       className="text-white text-4xl font-black leading-tight tracking-[-0.033em] @[480px]:text-5xl @[480px]:font-black @[480px]:leading-tight @[480px]:tracking-[-0.033em]"
                     >
-                      ToDoCoWorks
+                      todocoworks
                        <span className="block"></span>
                     </h1>
                     <h2 className="text-white text-sm font-normal leading-normal @[480px]:text-base @[480px]:font-normal @[480px]:leading-normal">
@@ -129,7 +129,7 @@ export default function HomePage() {
               </div>
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 p-0">
                 <div className="flex flex-1 gap-3 rounded-lg border border-[#dbdbdb] bg-neutral-50 p-4 flex-col">
-                  <div className="text-[#141414]" data-icon="Clock" data-size="24px" data-weight="regular">
+                  <div className="text-green-600" data-icon="Clock" data-size="24px" data-weight="regular">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
                       <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm64-88a8,8,0,0,1-8,8H128a8,8,0,0,1-8-8V72a8,8,0,0,1,16,0v48h48A8,8,0,0,1,192,128Z"></path>
                     </svg>
@@ -140,7 +140,7 @@ export default function HomePage() {
                   </div>
                 </div>
                 <div className="flex flex-1 gap-3 rounded-lg border border-[#dbdbdb] bg-neutral-50 p-4 flex-col">
-                  <div className="text-[#141414]" data-icon="UsersThree" data-size="24px" data-weight="regular">
+                  <div className="text-green-600" data-icon="UsersThree" data-size="24px" data-weight="regular">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
                       <path d="M244.8,150.4a8,8,0,0,1-11.2-1.6A51.6,51.6,0,0,0,192,128a8,8,0,0,1-7.37-4.89,8,8,0,0,1,0-6.22A8,8,0,0,1,192,112a24,24,0,1,0-23.24-30,8,8,0,1,1-15.5-4A40,40,0,1,1,219,117.51a67.94,67.94,0,0,1,27.43,21.68A8,8,0,0,1,244.8,150.4ZM190.92,212a8,8,0,1,1-13.84,8,57,57,0,0,0-98.16,0,8,8,0,1,1-13.84-8,72.06,72.06,0,0,1,33.74-29.92,48,48,0,1,1,58.36,0A72.06,72.06,0,0,1,190.92,212ZM128,176a32,32,0,1,0-32-32A32,32,0,0,0,128,176ZM72,120a8,8,0,0,0-8-8A24,24,0,1,1,87.24,82a8,8,0,1,0,15.5-4A40,40,0,1,0,37,117.51,67.94,67.94,0,0,0,9.6,139.19a8,8,0,1,0,12.8,9.61A51.6,51.6,0,0,1,64,128,8,8,0,0,0,72,120Z"></path>
                     </svg>
@@ -242,7 +242,7 @@ export default function HomePage() {
                   <h2
                     className="text-[#141414] tracking-light text-[22px] font-bold leading-tight @[480px]:text-4xl @[480px]:font-black @[480px]:leading-tight @[480px]:tracking-[-0.033em] max-w-[720px] mx-auto"
                   >
-                    業務を効率化をサポートします。
+                    業務の効率化をサポートします。
                   </h2>
                   <p className="text-[#141414] text-base font-normal leading-normal max-w-[720px] mx-auto">まずはご相談ください。</p>
                 </div>
